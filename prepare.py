02a269478257a646914dac7e129ad0f5f1a0a92c
OscarSchyns <38758071+OscarSchyns@users.noreply.github.com>
2019-03-26 15:15:18 +0100

    Update prepare.py

--- prepare.py
+++ prepare.py
@@ -1,5 +1,8 @@
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+ps = PorterStemmer()
 
 example_text = "Hello my name is Oscar steal stole stealing"
 stop_words = set(stopwords.words("english"))
@@ -7,4 +10,8 @@ stop_words = set(stopwords.words("english"))
 words = words_tokenize(example_text)
 
 filtered_sentence = [w for w in words if not w in stop_words]
-print(filtered_sentence) 
+print(filtered_sentence)
+
+for w in filtered_sentence:
+  print(ps.stem(w))
+
