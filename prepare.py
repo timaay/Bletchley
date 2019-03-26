c3c6bf6dd49448b1c3daabb6f6d689a8396521dd
OscarSchyns <38758071+OscarSchyns@users.noreply.github.com>
2019-03-26 15:10:13 +0100

    Update prepare.py

--- prepare.py
+++ prepare.py
@@ -2,3 +2,9 @@ from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 
 example_text = "Hello my name is Oscar steal stole stealing"
+stop_words = set(stopwords.words("english"))
+
+words = words_tokenize(example_text)
+
+filtered_sentence = [w for w in words if not w in stop_words]
+print(filtered_sentence) 
